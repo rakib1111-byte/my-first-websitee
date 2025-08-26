b194a0f812f18f389a52b94ba26737fb6b1e4ba0
rakib1111-byte <theamena36@gmail.com>
2025-08-26 21:31:25 +0600

    Add files via upload

---
+++ new1.py
@@ -0,0 +1,7 @@
+name = input("তোমার নাম লিখো: ")
+age = int(input("তোমার বয়স লিখো: "))
+color = input("তোমার প্রিয় রঙ লিখো: ")
+
+print("আমার নাম", name)
+print("আমার বয়স", age)
+print("আমার প্রিয় রঙ", color)
